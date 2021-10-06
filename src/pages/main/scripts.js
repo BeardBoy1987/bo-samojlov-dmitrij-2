b87fe397ab779c16c799fe5cabfb62dbead06205
Samoylov Dmitriy <78538544+BeardBoy1987@users.noreply.github.com>
2021-10-06 08:57:02 +0400

    some fixes

--- src/pages/main/scripts.js
+++ src/pages/main/scripts.js
@@ -1,23 +1,23 @@
 "use strict";
 
-var popupOpenButton = document.querySelector(".explore-btn");
+var popupOpenButton = document.querySelector(".button--explore");
 var popupWindow = document.querySelector(".popup-wrapper");
-var popupCloseButton = document.querySelector(".close-btn");
+var popupCloseButton = document.querySelector(".contact-form__close-btn");
 var inputFocus = popupWindow.querySelector("input");
 
 popupOpenButton.addEventListener("click", function () {
-  popupWindow.classList.add("popup-wrapper-open");
+  popupWindow.classList.add("popup-wrapper--open");
   inputFocus.focus();
 });
 
 popupCloseButton.addEventListener("click", function () {
-  popupWindow.classList.remove("popup-wrapper-open");
+  popupWindow.classList.remove("popup-wrapper--open");
   popupOpenButton.focus();
 });
 
 window.addEventListener("keydown", function (e) {
-  if (e.code === "Escape" && popupWindow.classList.contains("popup-wrapper-open")) {
-    popupWindow.classList.remove("popup-wrapper-open");
+  if (e.code === "Escape" && popupWindow.classList.contains("popup-wrapper--open")) {
+    popupWindow.classList.remove("popup-wrapper--open");
     popupOpenButton.focus();
   };
 });
